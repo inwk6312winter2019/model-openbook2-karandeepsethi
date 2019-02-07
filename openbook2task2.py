2754a4d9df017a65b89414640198927777d9c65a
sam smith <singhkd60@gmail.com>
2019-02-06 20:24:50 -0400

    this is the 2 and 3 part and task 2 is completed in this

--- openbook2task2.py
+++ openbook2task2.py
@@ -9,6 +9,29 @@ def accessible(d,data):
 					a=a+1
 	return a
 
+def non_standard(d,data):
+        b=0
+        for line in data:
+                if line[10]=="localstreet"
+                        fdmid=line[23]
+                for line1 in data:
+                        if line1[9]==fdmid
+                                line1[7]=="non-standard"
+                                        b=b+1
+        return b
+
+
+def inaccessible(d,data):
+        c=0
+        for line in data:
+                if line[10]=="Minor Collector"
+                        fdmid=line[23]
+                for line1 in data:
+                        if line1[9]==fdmid
+                                line1[7]=="inaccessible"
+                                        c=c+1
+        return c
+
 
 d=[]
 data=[]
@@ -22,26 +45,7 @@ for line in fout2:
 	data.append(line)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	fout1=open("Bus_Stops.csv",'r')
-	fout1.readline()
-	fout2=open("Street_Centrelines.csv",'r')
-	fout2.readline()
+print(accessible(d,data))
+print(non_standard(d,data))
+print(inaccessible(d,data))
 
