b9879f006c923b87b9c2bc2ae3e7aae7f1993a4e
sam smith <singhkd60@gmail.com>
2019-02-06 20:04:14 -0400

    this is the first part of task2

---
+++ openbook2task2.py
@@ -0,0 +1,47 @@
+def accessible(d,data):
+	a=0
+	for line in data:
+		if line[10]=="arterial"
+			fdmid=line[23]
+		for line1 in data:
+			if line1[9]==fdmid
+				line1[7]=="accessible"
+					a=a+1
+	return a
+
+
+d=[]
+data=[]
+fout1=open("Bus_Stops.csv")
+for line in fout1:
+	line=line.split(',')
+	data.append(line)
+fout2=open("Street_Centrelines.csv")
+for line in fout2:
+	line=line.split(',')
+	data.append(line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	fout1=open("Bus_Stops.csv",'r')
+	fout1.readline()
+	fout2=open("Street_Centrelines.csv",'r')
+	fout2.readline()
+
